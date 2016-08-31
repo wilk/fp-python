d8cd3ad0189d7eecde49bd7950eff261006bf154
Wilk <wilk3ert@gmail.com>
2016-08-31 22:35:58 +0200

    added reduce

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# transform an array
+# map
 
 ## procedural
 names = ['Mary', 'Isla', 'Sam']
@@ -13,3 +13,33 @@ names = ['Mary', 'Isla', 'Sam']
 
 print map(lambda x: hash(x), names)
 
+# reduce
+
+## procedural
+people = [{'name': 'Mary', 'height': 160},
+          {'name': 'Isla', 'height': 80},
+          {'name': 'Sam'}]
+
+height_total = 0
+height_count = 0
+for person in people:
+    if 'height' in person:
+        height_total += person['height']
+        height_count += 1
+
+if height_count > 0:
+    average_height = height_total / height_count
+
+    print average_height
+    # => 120
+
+## functional
+
+people_with_height = filter(lambda p: 'height' in p, people)
+height_count = len(people_with_height)
+height_total = reduce(lambda a, p: a + p['height'], people_with_height, 0)
+
+if height_count > 0:
+    average_height = height_total / height_count
+
+    print average_height
